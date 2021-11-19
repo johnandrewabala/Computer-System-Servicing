d838bf93a4be1843935ca14fdd9a60f8aba0b26d
John Andrew Abala <johnandrew.abala@deped.gov.ph>
2021-11-19 14:15:14 +0800

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-
-
-
-//////////////////////////////////////////// Source Code of CSS Class
-
 WA.chat.sendChatMessage('Good day! Welcome to CSS Class', 'Mr Abala');
 
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
@@ -10,9 +5,22 @@ WA.chat.sendChatMessage('Good day! Welcome to CSS Class', 'Mr Abala');
 import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
+
 bootstrapExtra().catch(e => console.error(e));
 
 let currentPopup: any = undefined;
+
+const today = new Date();
+const time = today.getHours() + ":" + today.getMinutes();
+
+WA.room.onEnterZone('clock', () => {
+    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+})
+
+WA.room.onLeaveZone('clock', closePopUp)
+
+}
+
 
 WA.room.onEnterZone('message', () => {
     currentPopup =  WA.ui.openPopup("popupRectangle","Welcome to CSS Room",[]);
@@ -26,23 +34,3 @@ function closePopUp(){
         currentPopup = undefined;
     }
 }
-
-//Clock Script 
-
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
-
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
-
-WA.room.onLeaveZone('clock', closePopUp)
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
-
-/////////////////////////////////////////////////////////////////// 

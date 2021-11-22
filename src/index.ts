edd9cfba357c1c829114e04001aab219319a99c9
John Andrew Abala <johnandrew.abala@deped.gov.ph>
2021-11-22 12:06:04 +0800

    update map and index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-
-
-
-//////////////////////////////////////////// Source Code of Map CCS
-
-WA.chat.sendChatMessage('Good day! Welcome to CSS Class', 'Mr Abala');
-
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+
+WA.chat.sendChatMessage('Welcome to Computer System Servicing Class!', 'Mr.Abala');
 
 import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 
@@ -14,30 +9,9 @@ bootstrapExtra().catch(e => console.error(e));
 
 let currentPopup: any = undefined;
 
-WA.room.onEnterZone('message', () => {
-    currentPopup =  WA.ui.openPopup("popupRectangle","Welcome to the CSS Room",[{
-        label: "Okay",
-        className: "normal",
-        callback: (popup) => {
-            // Close the popup when the "Close" button is pressed.
-            popup.close();
-        }
-    });
-}]);
-})
-
-WA.room.onLeaveZone('message', closePopUp)
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
-
-
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
+
 
 WA.room.onEnterZone('clock', () => {
     currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
@@ -45,9 +19,28 @@ WA.room.onEnterZone('clock', () => {
 
 WA.room.onLeaveZone('clock', closePopUp)
 
+WA.room.onEnterZone('clock1', () => {
+    currentPopup =  WA.ui.openPopup("clockPopup1","It's " + time,[]);
+})
+
+WA.room.onLeaveZone('clock1', closePopUp)
+
+
+WA.room.onEnterZone('nook', () => {
+    currentPopup =  WA.ui.openPopup("nookPopup","Welcome to Meeting Room! ",[]);
+})
+
+WA.room.onLeaveZone('nook', closePopUp)
+
+WA.room.onEnterZone('entrance', () => {
+    currentPopup =  WA.ui.openPopup("entrancePopup","Welcome to Computer System Servicing Laboratory! ",[]);
+})
+
+WA.room.onLeaveZone('entrance', closePopUp)
+
 function closePopUp(){
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
-    }
+   }
 }
